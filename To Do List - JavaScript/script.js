cf55f22fc803c7918f0dbb52e3f9d1fa5baadf4c
Anwar Hakim <anwarhakim31@gmail.com>
2024-03-14 12:14:22 +0700

    create function add task - To Do List

--- To Do List - JavaScript/script.js
+++ To Do List - JavaScript/script.js
@@ -1,44 +1,49 @@
 const btnadd = document.querySelector(".btn-add");
-const keyword = document.querySelector(".input-keyword");
-const taskcontainer = document.querySelector(".task-container");
+const inputkeyword = document.querySelector(".input-keyword");
 const notif = document.querySelector(".notif");
+const taskcontainer = document.querySelector(".task-container");
 
-btnadd.addEventListener("click", function (e) {
-  e.preventDefault();
-  let val = keyword.value;
-
-  if (val === "") {
-    displayAlert("Enter Your task!", "danger");
-  } else if (val) {
-    createTask(val);
-    displayAlert("Your task has been added!", "succes");
+btnadd.addEventListener("click", function () {
+  const KValue = inputkeyword.value;
+  if (KValue === "") {
+    showalert("Enter Your task!", "danger");
+  }
+  if (KValue.length > 80) {
+    showalert("too many letters, enter less than 80 letters", "letter");
+  } else if (KValue) {
+    createTask(KValue);
+    showalert("Your task has been added!", "succes");
   }
 
-  keyword.value = "";
+  KValue.innerHTML = "";
 });
 
-function createTask(val) {
+function createTask(KValue) {
   const li = document.createElement("li");
 
-  li.innerHTML = `<input type="checkbox" class="check" />
-                    <p class="text">${val}</p>
-                    <div class="button">
-                    <i class="bx bx-edit edit"></i>
-                    <i class="bx bxs-trash trash"></i>
-                    </div>`;
+  li.innerHTML = template(KValue);
 
   taskcontainer.appendChild(li);
 }
 
-function displayAlert(message, style) {
+function template(KValue) {
+  return `    
+                    <input type="checkbox" class="check" />
+                    <p class="text">${KValue}</p>
+                    <div class="button">
+                    <i class="bx bx-edit edit"></i>
+                    <i class="bx bxs-trash trash"></i>
+                    </div>
+                    `;
+}
+
+function showalert(message, style) {
   notif.innerHTML = message;
   notif.classList.add(style);
   notif.classList.remove("hidden");
+
   setTimeout(() => {
-    notif.classList.add("hidden");
     notif.classList.remove(style);
-    setTimeout(() => {
-      notif.innerHTML = "";
-    }, 450);
+    notif.classList.add("hidden");
   }, 1000);
 }
