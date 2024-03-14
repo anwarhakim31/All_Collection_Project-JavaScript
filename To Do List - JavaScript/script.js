01239ad4b575e317b1fff639d1da5312020de40f
Anwar Hakim <anwarhakim31@gmail.com>
2024-03-14 22:08:18 +0700

    create function edit - To Do List

--- To Do List - JavaScript/script.js
+++ To Do List - JavaScript/script.js
@@ -3,6 +3,11 @@ const inputkeyword = document.querySelector(".input-keyword");
 const notif = document.querySelector(".notif");
 const taskcontainer = document.querySelector(".task-container");
 
+const btnCancel = document.querySelector(".btn-cancel");
+
+let editflag = false;
+let editelement; ////untuk nilaimembuat variabel menjadi global
+
 //add task while we put value in text and press BUTTON
 btnadd.addEventListener("click", function () {
   const KValue = inputkeyword.value.trim(); //trim menghapus space di depan value dan balik ke depan input value // mengabaikan space di depan
@@ -11,25 +16,51 @@ btnadd.addEventListener("click", function () {
   }
   if (KValue.length > 80) {
     showalert("too many letters, enter less than 80 letters", "letter");
-  } else if (KValue) {
+  } else if (KValue && !editflag) {
     createTask(KValue);
     showalert("Your task has been added!", "succes");
+  } else if (KValue && editflag) {
+    editelement.textContent = KValue;
+    showalert("Your task has been Edit!", "succes");
   }
   inputkeyword.value = "";
+});
+
+btnCancel.addEventListener("click", function () {
+  editflag = false;
+  editelement = undefined;
+  const taskbutton = document.querySelectorAll(".button");
+  taskbutton.forEach((button) => {
+    button.classList.remove("d-none");
+  });
+  this.classList.remove("active");
+
+  const inputcheck = document.querySelectorAll(".input-check");
+  inputcheck.forEach((check) => check.classList.remove("d-none"));
+
+  btnadd.textContent = "Add";
+
+  const ul = document.querySelector("ul");
+  const lis = ul.childNodes;
+  lis.forEach((ul) => {
+    ul.style.background = "white";
+  });
 });
 
 function createTask(KValue) {
   const li = document.createElement("li");
 
   li.innerHTML = template(KValue);
-  taskcontainer.appendChild(li);
-  const text = li.children[1];
 
-  text.addEventListener("click", function () {
-    li.classList.toggle("licheck");
-    const checkbox = this.previousElementSibling;
-    checkbox.checked = checkbox.checked ? false : true;
-  });
+  taskcontainer.appendChild(li);
+  // const text = li.children[1];
+
+  // text.addEventListener("click", function () {
+  //   li.classList.toggle("licheck");
+
+  //   const checkbox = this.previousElementSibling;
+  //   checkbox.checked = checkbox.checked ? false : true;
+  // });
 }
 
 function deleteTask(e) {
@@ -42,16 +73,39 @@ function check(e) {
 }
 
 function editTask(e) {
-  const text = e.parentElement.previousElementSibling;
+  editflag = true;
+  const text = e.parentElement.previousElementSibling; //text
+  const li = text.parentElement; //li
+
   inputkeyword.value = text.textContent;
 
+  editelement = text;
+
   const btnCancel = document.querySelector(".btn-cancel");
-  btnCancel.classList.toggle("active");
+  btnCancel.classList.add("active");
+
+  const taskbutton = document.querySelectorAll(".button");
+  taskbutton.forEach((button) => {
+    button.classList.add("d-none");
+  });
+
+  const inputcheck = document.querySelectorAll(".input-check");
+  inputcheck.forEach((check) => check.classList.add("d-none"));
+
+  btnadd.textContent = "Edit";
+
+  const ul = document.querySelector("ul");
+  const lis = ul.childNodes;
+  lis.forEach((ul) => {
+    ul.style.background = "#414141bd";
+  });
+
+  li.style.background = "white";
 }
 
 function template(KValue) {
   return `    
-                    <input type="checkbox" name="status"  onclick="check(this)" />
+                    <input class="input-check" type="checkbox" name="status"  onclick="check(this)" />
                     <p class="text">${KValue}</p>
                     <div class="button">
                     <i class="bx bx-edit edit" onclick="editTask(this)"></i>
